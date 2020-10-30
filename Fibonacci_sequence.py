35ece8baa2a131c6f3f3cd49883be962d76e6485
as-uh <aloksinghmintorian@hotmail.com>
2020-10-30 00:50:51 +0000

    Create Fibonacci_sequence.py

---
+++ Fibonacci_sequence.py
@@ -0,0 +1,27 @@
+"""
+fibonacci function will take input for the number 
+of terms as nterms. 
+It will print user input number of terms of 
+fibonacci sequence."""
+
+def fibonacci(nterms):
+    # First and second term of the fibonacci Sequence
+    term0=0 
+    term1=1
+    
+    # Check if there is any term in the sequence
+    if nterms < 1:
+        print("Squence have no terms")
+    else:
+        print(f"First %i terms of Fibonacci sequence are:" 
+            %nterms)
+        for i in range(nterms):
+            print(term0)
+            nth_term = term0+term1
+            # Update values of the terms
+            term0 = term1
+            term1 = nth_term
+
+#test values
+fibonacci(5)
+fibonacci(10)
